102c682b588ad4dec4166f09317061deca27f5cd
RaselMridha792 <raselmridha792@gmail.com>
2025-02-21 06:12:13 +0600

    create post and get operation using query by email

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
       const result = await ToDoCollection.insertOne(userData)
       res.send(result)
     })
+
+    app.get('/user/:email', async(req, res)=>{
+      const email = req.params.email;
+      const query = {email: email}
+      const result = await ToDoCollection.find(query).toArray()
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
