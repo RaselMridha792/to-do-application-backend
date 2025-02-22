d43ea36019f39cab7efad1079e93a8248e795c29
RaselMridha792 <raselmridha792@gmail.com>
2025-02-22 16:05:56 +0600

    fix the undefined value issue

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-require("dotenv").config()
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.epzmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,69 +16,83 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    const ToDoCollection = client.db('to-do-application').collection('to-do-collection')
+    const ToDoCollection = client
+      .db("to-do-application")
+      .collection("to-do-collection");
     // await client.connect();
 
-
-    app.post('/tasks', async(req, res)=>{
+    app.post("/tasks", async (req, res) => {
       const userData = req.body;
-      const result = await ToDoCollection.insertOne(userData)
-      res.send(result)
-    })
+      const result = await ToDoCollection.insertOne(userData);
+      res.send(result);
+    });
 
-    app.get('/tasks/:email', async(req, res)=>{
+    app.get("/tasks/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {email: email}
-      const result = await ToDoCollection.find(query).toArray()
+      const query = { email: email };
+      const result = await ToDoCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    app.delete('/tasks/:id', async(req,res)=>{
+    app.delete("/tasks/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await ToDoCollection.deleteOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await ToDoCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-    app.put('/tasks/:id', async(req, res)=>{
+    app.put("/tasks/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: false };
+
+      const updatedFields = {};
+      if (data.taskName !== undefined) {
+        updatedFields.taskName = data.taskName;
+      }
+      if (data.description !== undefined) {
+        updatedFields.description = data.description;
+      }
+      if (data.category !== undefined) {
+        updatedFields.category = data.category;
+      }
+      if (data.date !== undefined) {
+        updatedFields.date = data.date;
+      }
       const updatedDoc = {
         $set: {
-          taskName: data.taskName,
-          description: data.description,
-          category: data.category,
-          date: data.date
-        }
-      }
-      const result = await ToDoCollection.updateOne(filter, updatedDoc, options)
+          updatedFields,
+        },
+      };
+      const result = await ToDoCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
-
-
-    })
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', async(req, res)=>{
-      res.send('to do application is running')
-})
+app.get("/", async (req, res) => {
+  res.send("to do application is running");
+});
 
-app.listen(port, ()=>{
-      console.log(`application is running on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`application is running on port:${port}`);
+});
